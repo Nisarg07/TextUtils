ec91cb6c1a81218e0a2255cc0ce9415a1940026b
Nisarg07 <53267005+Nisarg07@users.noreply.github.com>
2024-01-08 21:23:03 -0500

    Updates

---
+++ precache-manifest.94e8c33d5c6698a4acfb024d5d65a771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd3153b634bd6981d9089fdd90a788c",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "6a7642c2915f13e9141f",
+    "url": "/TextUtils/static/css/main.9db9e043.chunk.css"
+  },
+  {
+    "revision": "8c897e56c5df96b02a6c",
+    "url": "/TextUtils/static/js/2.a67da17c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils/static/js/2.a67da17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd36783d8e3ec488df7",
+    "url": "/TextUtils/static/js/3.51cdb4ee.chunk.js"
+  },
+  {
+    "revision": "6a7642c2915f13e9141f",
+    "url": "/TextUtils/static/js/main.968bc48e.chunk.js"
+  },
+  {
+    "revision": "ad70968dd1d1ad06c2e4",
+    "url": "/TextUtils/static/js/runtime-main.349b9ee9.js"
+  }
+]);
