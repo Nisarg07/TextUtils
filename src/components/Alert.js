4a903f1720bd84de13084db82ba11fc0134d050f
Nisarg07 <53267005+Nisarg07@users.noreply.github.com>
2024-01-11 18:59:12 -0500

    bugs fixed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,15 +6,17 @@ function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
-    props.alert && (
-      <div
-        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-        role="alert"
-        style={{ position: "fixed", width: "100%" }}
-      >
-        <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
-      </div>
-    )
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+          // style={{ position: "fixed", width: "100%", zIndex: "1000" }}
+        >
+          <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
+        </div>
+      )}
+    </div>
   );
 }
 
